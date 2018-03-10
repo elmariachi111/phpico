0ed19846156f13395c21bc95071c63fd7dcb3776
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2018-03-11 00:59:26 +0100

    static / self variables are NOT memorized.

--- app/html/index.php
+++ app/html/index.php
@@ -1,7 +1,25 @@
+<pre>
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
 
 $helloWorld = new CoB\HelloWorld();
-echo $helloWorld->sayHello();
 
+echo "Hello, my instance's x is: " . $helloWorld->whatsMyX() . PHP_EOL;
+echo "Hello, our class' i is: " . $helloWorld->whatsOurI() . PHP_EOL;
+echo "Hello, our counter is: " . \CoB\HelloWorld::whatsOurC() . PHP_EOL;
+echo "inline static test: " . $helloWorld->staticInline();
+
+echo " --- ADVERTISEMENT ---" . PHP_EOL;
+$helloWorld->incrementMyX();
+$helloWorld->incrementMyI();
+\CoB\HelloWorld::incrementOurC();
+
+echo "Hello, my instance's x is: " . $helloWorld->whatsMyX() . PHP_EOL;
+echo "Hello, our class' i is: " . $helloWorld->whatsOurI() . PHP_EOL;
+echo "Hello, our counter is: " .  \CoB\HelloWorld::whatsOurC() . PHP_EOL;
+echo "inline static test: " . $helloWorld->staticInline();
+
+
+?>
+</pre>
